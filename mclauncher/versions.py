d03401b874c76152015033efd41f2bf5a9edf0c6
Allen Sun <allen.haha@hotmail.com>
2023-07-22 12:33:03 +0800

    add request

--- mclauncher/versions.py
+++ mclauncher/versions.py
@@ -1,8 +1,10 @@
-
+import requests
 
 VERSIONS_URL = "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json"
 
 class MinecraftVersion():
     @classmethod
     def get_all(cls):
-        
+        versions = requests.get(VERSIONS_URL)
+        versions.raise_for_status()
+        return(versions.json) # Not finished, return json for now, return a MinecraftVersion list/dict later
