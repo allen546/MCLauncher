93a2e71a8fec42b47d864962d8b4545f092f3dd2
Allen Sun <allen.haha@hotmail.com>
2023-07-24 21:30:14 +0800

    mcbbs.net: http 403

---
+++ mclauncher/mods/mcbbs/mod.py
@@ -0,0 +1,6 @@
+import bs4, requests
+
+
+URL = "https://search.mcbbs.net/forum.php?mod=forumdisplay&fid=45&filter=sortall&sortall=1" # www.mcbbs.net 403, but search.mcbbs.net seems to be the same site without 403
+def fetch():
+    pass
