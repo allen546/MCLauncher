15d2b6bde573584924f72fb4d4d15fdbf1c56b25
Allen Sun <allen.haha@hotmail.com>
2023-07-24 21:41:48 +0800

    add a generator modlist fetcher, no beautiful soup code yet

--- mclauncher/mods/mcbbs/mod.py
+++ mclauncher/mods/mcbbs/mod.py
@@ -1,6 +1,13 @@
 import bs4, requests
 
 
-URL = "https://search.mcbbs.net/forum.php?mod=forumdisplay&fid=45&filter=sortall&sortall=1" # www.mcbbs.net 403, but search.mcbbs.net seems to be the same site without 403
-def fetch():
-    pass
+MODS_URL = "https://search.mcbbs.net/forum.php?mod=forumdisplay&fid=45&sortall=1&sortall=1&filter=sortall&page={pagenum}" 
+# www.mcbbs.net 403, but search.mcbbs.net seems to be the same site without 403
+def fetch_modlist():
+    for i in range(1, 101):
+        r = requests.get(MODS_URL.format(i))
+        r.raise_for_status()
+        # beautifulsoup filter: table#threadlisttableid > th.common
+        
+        
+        yield r # use a generator to provide more and more pages when required
