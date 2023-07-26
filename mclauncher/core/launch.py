5be156dd329e0c34fb072533bfa8470365043b25
Allen Sun <allen.haha@hotmail.com>
2023-07-26 22:00:12 +0800

    add get_startup_options()

--- mclauncher/core/launch.py
+++ mclauncher/core/launch.py
@@ -1,1 +1,11 @@
 import portablemc
+
+
+
+def get_startup_options(authsession_or_username, resolution=None):
+    opts = portablemc.StartOptions()
+    opts.resolution = resolution if resolution else (1366,768)
+    if isinstance(authsession_or_username, str):
+        opts.username = authsession_or_username
+    else:
+        opts.auth_session = authsession_or_username
