016358d1e5f64be99da1c4bb60610aaad1950a8c
Allen Sun <allen.haha@hotmail.com>
2023-07-30 09:35:47 +0800

    add install-jdk

--- mclauncher/core/jvm.py
+++ mclauncher/core/jvm.py
@@ -1,8 +1,8 @@
 import os.path
-from jdk import install
+from .installjdk import install
 
 def install_all_used_java(path="~/.jdk/"):
     path = os.path.expanduser(path)
-    install("8u351", jre=True, path=os.path.join(path, "8u351"))
-    install("11", jre=True, path=os.path.join(path, "11"))
-    install("17", jre=True, path=os.path.join(path, "17"))
+    #install("8u351", jre=True, path=os.path.join(path, "8u351"), impl="adoptium")
+    install("11", jre=True, path=os.path.join(path, "11"), impl="adoptium")
+    install("17", jre=True, path=os.path.join(path, "17"), impl="adoptium")
